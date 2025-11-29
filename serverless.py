791bd75fc24e08686cec1ed6f606de3e3e028b28
trpl333 <jpeterson@bidprep.com>
2025-11-29 08:23:59 -0800

    Fix import statement for handler in serverless.py

--- serverless.py
+++ serverless.py
@@ -1,5 +1,6 @@
 import runpod
 
-from .handler import handler
+# import the handler from the same directory (absolute import when running as a script)
+from handler import handler
 
 runpod.serverless.start({"handler": handler})
